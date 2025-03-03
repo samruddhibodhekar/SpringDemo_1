61d8b049ef0edbc6c37d02b5b68219ac1190cf8e
rudra <rudra@RudraSavi>
2025-03-03 12:09:57 +0530

    SpringBoot Student Curd Operation

---
+++ StudentCurd/src/main/java/com/cjc/curd/main/controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.cjc.curd.main.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController
+{
+	
+
+}
---
+++ StudentCurd/src/test/java/com/cjc/curd/main/StudentCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.curd.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
