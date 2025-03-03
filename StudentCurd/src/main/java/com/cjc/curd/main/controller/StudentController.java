6bc7d1f69b6e1e7623e7303e4fee3f6a7bc31ddc
sushantkhilare25 <sskhilare25@gmail.com>
2025-03-03 12:32:47 +0530

    Add Post Method

--- StudentCurd/src/main/java/com/cjc/curd/main/controller/StudentController.java
+++ StudentCurd/src/main/java/com/cjc/curd/main/controller/StudentController.java
@@ -1,10 +1,19 @@
 package com.cjc.curd.main.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.curd.main.model.Student;
 
 @RestController
 public class StudentController
 {
-	
+	@PostMapping("/postData")
+	public String postData(@RequestBody Student s)
+	{
+		
+		return "Update Data";
+	}
 
 }
